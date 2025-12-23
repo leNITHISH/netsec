61678050a1ea3dbe7d75a7d8643701e9c1cc39df
Nithish <the.nithish.siva.kumar@gmail.com>
2025-12-23 20:40:42 +0530

    ex1b completed

--- ex2/play_reciever.py
+++ ex2/play_reciever.py
@@ -1,27 +1,41 @@
-import socket
+import numpy as np;
+import socket;
 
-def reciever():
-    reci = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
+def receiver():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
+    s.bind(("localhost", 42069));
+    s.listen(1);
+    print("waiting...");
+    c, addr = s.accept();
+    msg = c.recv(1024).decode("ascii");
+    print(f"Cipher={msg}");
+    print("enter key:");
+    k = input();
+    dec = pf_d(msg, k);
+    print(f"Decrypted={dec}");
+    c.close();
 
-    reci.bind(("localhost", 42069));
-    reci.listen(1);
-    print("Listenin'...");
-    conn, addr = reci.accept();
-    print("Connected!");
+def pf_d(txt, k):
+    k = k.upper().replace("J", "I") + "ABCDEFGHIKLMNOPQRSTUVWXYZ";
+    u = [];
+    [u.append(x) for x in k if x not in u and x.isalpha()];
+    m = np.array(u).reshape(5, 5);
 
-    payload = conn.recv(1024).decode("ascii");
-    print(f"encrypted message: \"{payload}\"\n\nenter key to decrypt:");
-    key = int(input());
-    msd = caesar_d(payload, key);
-    print(f"recieved message: \"{msd}\"");
+    out = "";
+    for i in range(0, len(txt), 2):
+        a, b = txt[i], txt[i+1];
+        p1 = np.where(m == a);
+        r1, c1 = p1[0][0], p1[1][0];
+        p2 = np.where(m == b);
+        r2, c2 = p2[0][0], p2[1][0];
 
-    conn.close();
-    reci.close();
+        if r1 == r2:
+            out += m[r1, (c1-1)%5] + m[r2, (c2-1)%5];
+        elif c1 == c2:
+            out += m[(r1-1)%5, c1] + m[(r2-1)%5, c2];
+        else:
+            out += m[r1, c2] + m[r2, c1];
+            
+    return out;
 
-def caesar_d(string, key):
-    ret = [];
-    for c in string:
-        ret.append(chr((ord(c)-key)%128));
-
-    return "".join(ret);
-reciever();
+receiver();
